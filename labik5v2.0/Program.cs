72ede94da64e89ec4ae6ada5baa4334972e2c4f3
Andrew <Andrew@DESKTOP-M5S864B>
2023-12-04 02:21:21 +0200

    Добавьте файлы проекта.

---
+++ labik5v2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Створення дороги
+        Road cityRoad = new Road(10, 4, 2, 3);
+
+        // Створення транспортних засобів
+        List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Vehicle(60, 2, VehicleType.Car),
+            new Vehicle(40, 4, VehicleType.Truck),
+            new Vehicle(50, 3, VehicleType.Bus)
+        };
+
+        // Симуляція руху на дорозі
+        TrafficSimulation.SimulateTraffic(cityRoad, vehicles);
+    }
+}
---
+++ labik5v2.0/Road.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Клас "Дорога"
+class Road
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public int NumberOfLanes { get; set; }
+    public int TrafficLevel { get; set; }
+
+    public Road(double length, double width, int numberOfLanes, int trafficLevel)
+    {
+        Length = length;
+        Width = width;
+        NumberOfLanes = numberOfLanes;
+        TrafficLevel = trafficLevel;
+    }
+}
+
+// Енумерація для типу транспортного засобу
+enum VehicleType
+{
+    Car,
+    Truck,
+    Bus
+}
+
+// Клас "Транспортний засіб"
+class Vehicle : IDriveable
+{
+    public double Speed { get; set; }
+    public double Size { get; set; }
+    public VehicleType Type { get; set; }
+
+    public Vehicle(double speed, double size, VehicleType type)
+    {
+        Speed = speed;
+        Size = size;
+        Type = type;
+    }
+
+    public void Move()
+    {
+        Console.WriteLine($"The {Type} is moving at a speed of {Speed} km/h.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine($"The {Type} has stopped.");
+    }
+}
+
+// Інтерфейс для дій транспортного засобу
+interface IDriveable
+{
+    void Move();
+    void Stop();
+}
+
+// Клас для імітації руху та зміни руху транспортних засобів
+class TrafficSimulation
+{
+    public static void SimulateTraffic(Road road, List<Vehicle> vehicles)
+    {
+        Console.WriteLine($"Simulating traffic on a road with {road.NumberOfLanes} lanes and length {road.Length} km.");
+
+        foreach (var vehicle in vehicles)
+        {
+            Console.WriteLine($"Vehicle Type: {vehicle.Type}, Size: {vehicle.Size}");
+            vehicle.Move();
+        }
+    }
+}
+
